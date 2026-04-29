cf9556b41b4a1e530c43dbddd6cf26e1bf832e3c
wilsonthomas215-arch <wilsonthomas215@gmail.com>
2026-04-29 11:53:11 -0400

    Add styles configuration

---
+++ styles.py
@@ -0,0 +1,15 @@
+# Screen dimensions
+WIDTH = 390
+HEIGHT = 844
+
+# Colors
+BG_COLOR = (15, 15, 35)  # Dark blue-black
+CARD_COLOR = (40, 40, 70)  # Dark blue
+ACCENT = (255, 107, 107)  # Coral/Red
+TEXT_COLOR = (255, 255, 255)  # White
+SECONDARY_TEXT = (180, 180, 200)  # Light gray
+
+# Fonts
+FONT_SIZE_LARGE = 24
+FONT_SIZE_MEDIUM = 20
+FONT_SIZE_SMALL = 16

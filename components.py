742b96d6df81edc7dd957adb3d2f69c4db706a0d
wilsonthomas215-arch <wilsonthomas215@gmail.com>
2026-04-29 11:54:08 -0400

    Add UI components

---
+++ components.py
@@ -0,0 +1,20 @@
+from PIL import ImageFont
+from styles import *
+
+def draw_card(draw, x, y, width, height, color):
+    """Draw a rounded rectangle card"""
+    draw.rectangle(
+        [(x, y), (x + width, y + height)],
+        fill=color,
+        outline=SECONDARY_TEXT,
+        width=1
+    )
+
+def draw_text(draw, text, x, y, color, size=FONT_SIZE_MEDIUM):
+    """Draw text on the image"""
+    try:
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", size)
+    except:
+        font = ImageFont.load_default()
+    
+    draw.text((x, y), text, fill=color, font=font)
